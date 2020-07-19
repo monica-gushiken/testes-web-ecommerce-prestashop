a835a4856764195d302f0c69ed45246d9bb36bef
Monica Mayumi Gushiken <monica.gushiken@gmail.com>
2020-07-19 19:05:01 -0300

    Incluindo feature e ajustando para utilização do Cucumber.

---
+++ src/test/java/steps/ComprarProdutoSteps.java
@@ -0,0 +1,141 @@
+package steps;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.ModalProdutoPage;
+import pages.ProdutoPage;
+import util.Funcoes;
+
+public class ComprarProdutoSteps {
+
+	private static WebDriver driver;
+	private HomePage homePage = new HomePage(driver);
+
+	@Before
+	public static void inicializar() {
+		System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver\\83\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@Dado("que estou na pagina inicial")
+	public void que_estou_na_pagina_inicial() {
+		homePage.carregarPaginaInicial();
+		assertEquals("Loja de Teste", homePage.obterTituloPagina());
+	}
+
+	@Quando("nao estou logado")
+	public void nao_estou_logado() {
+		assertEquals(false, homePage.estaLogado());
+	}
+
+	@Entao("visualizo {int} produtos disponiveis")
+	public void visualizo_produtos_disponiveis(Integer int1) {
+		assertEquals(int1, homePage.contarProdutos());
+	}
+
+	@Entao("carrinho esta zerado")
+	public void carrinho_esta_zerado() {
+		assertEquals(0, homePage.obterQuantidadeProdutosNoCarrinho());
+	}
+
+	LoginPage loginPage;
+	String email = "monica@teste.com";
+
+	@Quando("estou logado")
+	public void estou_logado() {
+		loginPage = homePage.clicarBotaoSignIn();
+		loginPage.preencherEmail(email);
+		loginPage.preencherSenha("123456");
+		loginPage.clicarBotaoSignIn();
+		assertEquals(true, homePage.estaLogado("Monica Teste"));
+		homePage.carregarPaginaInicial();
+	}
+
+	ProdutoPage produtoPage;
+	String nomeProduto_HomePage;
+	String precoProduto_HomePage;
+	String nomeProduto_ProdutoPage;
+	String precoProduto_ProdutoPage;
+
+	@Quando("seleciono um produto na posicao {int}")
+	public void seleciono_um_produto_na_posicao(Integer indice) {
+		nomeProduto_HomePage = homePage.obterNomeProduto(indice);
+		precoProduto_HomePage = homePage.obterPrecoProduto(indice);
+
+		produtoPage = homePage.clicarProduto(indice);
+
+		nomeProduto_ProdutoPage = produtoPage.obterNomeProduto();
+		precoProduto_ProdutoPage = produtoPage.obterPrecoProduto();
+	}
+
+	@Quando("nome do produto na tela principal e na tela produto eh {string}")
+	public void nome_do_produto_na_tela_principal_eh(String nomeProduto) {
+		assertEquals(nomeProduto_HomePage.toUpperCase(), nomeProduto.toUpperCase());
+		assertEquals(nomeProduto_ProdutoPage.toUpperCase(), nomeProduto.toUpperCase());
+	}
+
+	@Quando("preco do produto na tela principal e na tela produto eh {string}")
+	public void preco_do_produto_na_tela_principal_eh(String precoProduto) {
+		assertEquals(precoProduto_HomePage, precoProduto);
+		assertEquals(precoProduto_ProdutoPage, precoProduto);
+	}
+
+	ModalProdutoPage modalProdutoPage;
+
+	@Quando("adiciono o produto no carrinho com tamanho {string} cor {string} e quantidade {int}")
+	public void adiciono_o_produto_no_carrinho_com_tamanho_cor_e_quantidade(String tamanhoProduto, String corProduto,
+			Integer quantidadeProduto) {
+		// Selecionar tamanho
+		List<String> listaOpcoes = produtoPage.obterOpcoesSelecionadas();
+		produtoPage.selecionarOpcaoDropdown(tamanhoProduto);
+
+		// Selecionar cor
+		produtoPage.selecionarCorPreta();
+
+		// Selecionar a quantidade
+		produtoPage.alterarQuantidade(quantidadeProduto);
+
+		// Clicar no botao add to cart
+		modalProdutoPage = produtoPage.clicarBotaoAddToCart();
+
+		// Validacoes
+		assertTrue(modalProdutoPage.obterMensagemProdutoAdicionado()
+				.endsWith("Product successfully added to your shopping cart"));
+	}
+
+	@Entao("o produto aparece na confirmacao com nome {string} preco {string} tamanho {string} cor {string} e quantidade {int}")
+	public void o_produto_aparece_na_confirmacao_com_nome_preco_tamanho_cor_e_quantidade(String nomeProduto,
+			String precoProduto, String tamanhoProduto, String corProduto, Integer quantidadeProduto) {
+		assertEquals(nomeProduto_ProdutoPage.toUpperCase(), modalProdutoPage.obterNomeProduto().toUpperCase());
+		assertEquals(tamanhoProduto, modalProdutoPage.obterTamanhoProduto());
+		assertEquals(corProduto, modalProdutoPage.obterCorProduto());
+		assertEquals(quantidadeProduto, modalProdutoPage.obterQuantidadeProduto());
+		Double precoProdutoEncontrado = Funcoes.removeCifraoDevolveDouble(modalProdutoPage.obterPrecoProduto());
+		Double precoProdutoEsperado = Funcoes.removeCifraoDevolveDouble(precoProduto);
+		Double subtotalEncontrado = Funcoes.removeCifraoDevolveDouble(modalProdutoPage.obterSubtotal());
+
+		Double subtotalCalculadoEsperado = quantidadeProduto * precoProdutoEsperado;
+
+		assertEquals(subtotalCalculadoEsperado, subtotalEncontrado);
+	}
+
+	@After
+	public static void finalizar() {
+		driver.quit();
+	}
+}
--- src/test/resources/features/visualiza_produtos.feature
+++ src/test/resources/features/visualiza_produtos.feature
@@ -1,11 +1,21 @@
 # language: pt
-Funcionalidade: Visualizar produtos
-  Como um usuario nao logado
-  Eu quero visualizar produtos disponiveis
-  Para poder escolher qual eu vou comprar
+Funcionalidade: Comprar produto
+  Como um usuario logado
+  Eu quero escolher um produto
+  e visualizar esse produto no carrinho
+  Para concluir o pedido
 
   Cenario: Deve mostrar uma lista de oito produtos na pagina inicial
     Dado que estou na pagina inicial
     Quando nao estou logado
     Entao visualizo 8 produtos disponiveis
     E carrinho esta zerado
+
+  Cenario: Deve mostrar produto escolhido confirmado
+    Dado que estou na pagina inicial
+    Quando estou logado
+    E seleciono um produto na posicao 0
+    E nome do produto na tela principal eh "Hummingbird Printed T-Shirt"
+    E preco do produto na tela principal eh "$19.12"
+    E adiciono o produto no carrinho com tamanho "M" cor "Black" e quantidade 2
+    Entao o produto aparece na confirmacao com nome "Hummingbird Printed T-Shirt" preco "$19.12" tamanho "M" cor "Black" e quantidade 2
